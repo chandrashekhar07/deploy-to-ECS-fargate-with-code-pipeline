241c85e1bf3ebe4e37ee06838ad51b9a9bd4bf89
Chandra Shekhar Neupane <35414590+chandrashekhar07@users.noreply.github.com>
2023-10-27 20:51:19 +0545

    feat: only keep the last 5 images in ecr (#1)

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,3 +2,25 @@ resource "aws_ecr_repository" "ecr" {
   name = var.ecr_name
 
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+  repository = aws_ecr_repository.ecr.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Only keep the last 5 images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
